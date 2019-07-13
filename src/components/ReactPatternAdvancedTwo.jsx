4c91658e203fe3bed9080b6d88694a090a203e51
Basel Munawwar <basel84mn@gmail.com>
2019-07-13 14:13:18 +0200

    pattern 2 B C

--- src/components/ReactPatternAdvancedTwo.jsx
+++ src/components/ReactPatternAdvancedTwo.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Switch from "./Switch";
+const CallAll = (...fns) => (...args) =>
+  fns.forEach((fn) => {
+    fn && fn(args);
+  });
 
 class Toggle extends Component {
   state = {
@@ -16,11 +20,41 @@ class ToggleB extends Component {
     on: true
   };
   toggle = () => this.setState(({ on }) => ({ on: !on }));
+  getStateAndHelpers = () => {
+    return {
+      on: this.state.on,
+      toggle: this.toggle,
+      togglerProps: {
+        onClick: this.toggle,
+        "aria-expanded": this.state.on
+      }
+    };
+  };
   render() {
-    return this.tsPropertySignature.children();
+    return this.props.children(this.getStateAndHelpers());
   }
 }
 
+class ToggleC extends Component {
+  state = {
+    on: true
+  };
+  toggle = () => this.setState(({ on }) => ({ on: !on }));
+  getStateAndHelpers = () => {
+    return {
+      on: this.state.on,
+      getTogglerProps: ({ onClick, ...props }) => {
+        return {
+          onClick: CallAll(this.toggle, onClick),
+          ...props
+        };
+      }
+    };
+  };
+  render() {
+    return this.props.children(this.getStateAndHelpers());
+  }
+}
 export default class ReactPatternAdvancedTwo extends Component {
   render() {
     return (
@@ -43,6 +77,44 @@ export default class ReactPatternAdvancedTwo extends Component {
             </>
           )}
         </Toggle>
+        <hr />
+        <h1>Pattern 2-B</h1>
+        <ToggleB>
+          {({ on, togglerProps }) => (
+            <>
+              <div>{on ? "On" : "Off"}</div>
+              <Switch on={on} {...togglerProps} />
+              <div style={{ padding: 10 }}>
+                <button
+                  style={{ padding: "10px 20px", borderRadius: 10 }}
+                  {...togglerProps}
+                >
+                  {on ? "On" : "Off"}
+                </button>
+              </div>
+            </>
+          )}
+        </ToggleB>
+        <hr />
+        <h1>Pattern 2-C</h1>
+        <ToggleC>
+          {({ on, getTogglerProps }) => (
+            <>
+              <div>{on ? "On" : "Off"}</div>
+              <Switch {...getTogglerProps({ on })} />
+              <div style={{ padding: 10 }}>
+                <button
+                  {...getTogglerProps({
+                    onClick: () => alert(on)
+                  })}
+                  style={{ padding: "10px 20px", borderRadius: 10 }}
+                >
+                  {on ? "On" : "Off"}
+                </button>
+              </div>
+            </>
+          )}
+        </ToggleC>
         <hr />
       </>
     );
